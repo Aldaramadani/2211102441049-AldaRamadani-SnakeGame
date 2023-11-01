19bc2933b2b1a9ff9034c4e8c13854e1b8efb9bc
Aldaramadani <145569864+Aldaramadani@users.noreply.github.com>
2023-11-01 14:52:42 +0800

    Add files via upload

---
+++ AldaRamadani-SnakeGame/Makanan.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  
+
+public class Makanan extends Actor
+{
+    public void addedToWorld(World Latar)
+    {
+        
+    }
+    
+    private void setGambar(int d)
+    {
+        
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
